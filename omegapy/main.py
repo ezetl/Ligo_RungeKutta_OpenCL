90f50c23e75a5d803afe08eecf85b1badd2082e5
ezetlopez <ezetl91@gmail.com>
2014-05-25 11:02:51 -0300

    shebang main py

--- omegapy/main.py
+++ omegapy/main.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 # coding: utf-8
 import sys
 from ode45 import Ode45
 
 def main():
-    ode45 = Ode45(batch=2)
+    ode45 = Ode45(batch=1)
     ode45.execute()
 
 if __name__ == "__main__":
